1b7fb55ab40edbbc7b5e3ec542af5bc5ca395786
omega <omega177@mail.ru>
2025-06-25 20:02:41 +0300

    true3

---
+++ src/main/java/com/example/demo/services/ProgectServices.java
@@ -0,0 +1,29 @@
+package com.example.demo.services;
+
+import com.example.demo.dto.ProjectDto;
+import com.example.demo.mappers.ProjectMapper;
+import com.example.demo.model.Project;
+import com.example.demo.repository.ProjectRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@AllArgsConstructor
+public class ProgectServices {
+    private  final ProjectRepository repository;
+    private final ProjectMapper mapper;
+
+
+    public ProjectDto getProgectById (Integer id){
+        Project project = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Project not found"));
+        return mapper.toDto(project);
+    }
+    public List<ProjectDto> getAllProgect(){
+        List<Project> projects = repository.findAll();
+        projects.forEach(System.out::println);
+        return mapper.toDto(projects);
+    }
+}
