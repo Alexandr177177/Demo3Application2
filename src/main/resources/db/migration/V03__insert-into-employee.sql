8d8236ca501e6638f177dd0e5c6ad0e7190c763d
omega <omega177@mail.ru>
2025-07-07 20:38:31 +0300

    true11 07.07.2025

---
+++ src/main/resources/db/migration/V03__insert-into-employee.sql
@@ -0,0 +1,3 @@
+INSERT INTO Employee (id, name, image, telephon, mail,english_level)
+VALUES (37, 'Olia1', '/images/41.jpg', 37429384435, 'olia@mail.ru',2),
+       (36, 'Veronika1', '/images/42.jpg', 3748762465, 'veronica@mail.ru ',1);
