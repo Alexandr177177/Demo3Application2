e5e0f339677d712cb582556bbcff1fc5c79630fb
omega <omega177@mail.ru>
2025-08-25 19:22:46 +0300

    true12 21.07.2025

---
+++ src/main/java/com/example/demo/request/MailRequest.java
@@ -0,0 +1,16 @@
+package com.example.demo.request;
+
+import com.example.demo.model.FormatFiles;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class MailRequest {
+    @NotBlank
+    @Email
+    String mailAddress;
+    @NotBlank
+    FormatFiles formatFile;
+
+}
--- src/main/java/com/example/demo/restcontrollers/EmployeeController.java
+++ src/main/java/com/example/demo/restcontrollers/EmployeeController.java
@@ -1,24 +1,32 @@
 package com.example.demo.restcontrollers;
+
 import com.example.demo.dto.EmployeeDto;
 import com.example.demo.dto.ProjectDto;
 import com.example.demo.model.Employee;
+import com.example.demo.request.MailRequest;
 import com.example.demo.services.EmployeeService;
+import com.example.demo.services.MailSenderService;
 import com.example.demo.services.ProjectService;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Optional;
 
+import static com.example.demo.model.FormatFiles.DOCX;
+import static com.example.demo.model.FormatFiles.XLSX;
+import static org.aspectj.weaver.tools.cache.SimpleCacheFactory.path;
 import static org.hibernate.Hibernate.map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/employees")
 public class EmployeeController {
-
+    private final MailSenderService mailSender;
     private final EmployeeService service;
 
 
@@ -41,6 +49,7 @@ public class EmployeeController {
             return ResponseEntity.notFound().build();
         }
     }
+
     @GetMapping("/by-mail")
     public ResponseEntity<EmployeeDto> getEmployeeByEmail(@RequestParam String mail) {
         try {
@@ -51,4 +60,47 @@ public class EmployeeController {
 
         }
     }
+
+    @PostMapping("/{id}/xls")
+    public ResponseEntity<String> createXLSFile(@PathVariable Integer id) {
+        EmployeeDto employee = service.getEmployeeById(id);
+        if (employee != null) {
+            mailSender.createXLSFile(employee);
+            return ResponseEntity.ok("File " + employee.getId() + " created");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/{id}/doc")
+    public ResponseEntity<String> createDocFile(@PathVariable Integer id) {
+        EmployeeDto employee = service.getEmployeeById(id);
+        if (employee != null) {
+            mailSender.createDOCFile(employee);
+            return ResponseEntity.ok("File " + employee.getId() + " created");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/{id}/attachmentMail")
+    public ResponseEntity<String> sendEmailWithAttachment(
+            @PathVariable Integer id,
+            @RequestBody @Valid MailRequest mailRequest
+    ) {
+        EmployeeDto employee = service.getEmployeeById(id);
+        String fileName = switch (mailRequest.getFormatFile()) {
+            case DOCX -> mailSender.createDOCFile(employee);
+            case XLSX -> mailSender.createXLSFile(employee);
+        };
+            //String path = mailSender.createDOCFile(employee);
+            mailSender.sendMailWithAttachment(
+                    mailRequest.getMailAddress(),
+                    "resume of " + employee.getName(),
+                    "Вы можите ознакомиться с содержанием письма",
+                    fileName
+            );
+            return ResponseEntity.ok("Email had sent to " + employee.getName());
+    }
 }
+
